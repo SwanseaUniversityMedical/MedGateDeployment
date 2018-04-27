3079d0a5cf58fc86fa5393c84e1eb28fe89aca41
Simon Thompson <simon@chi.swan.ac.uk>
2018-04-27 21:36:46 +0100

    Initial version on GitHub

---
+++ run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# stop all the running services
+if [[ -s ~/MedGateDeployment/docker/docker-compose.yml ]]
+then
+    docker-compose -f ~/MedGateDeployment/docker/docker-compose.yml stop
+fi
+
+# create gcp related directories for initialization 
+mkdir -p /gcp/instances/gcp_1/in
+mkdir -p /gcp/applications
+mkdir -p /gcp/data
+mkdir -p /gcp/output
+
+cd ~/MedGateDeployment/docker
+
+#remove medgate-service container and pull the latest medgate-service image 
+if [ "$(docker ps -aq -f name='medgate-service')" ]; then
+    # cleanup
+    docker-compose rm -f medgate-service
+    # pull the latest image
+    docker-compose pull medgate-service
+fi
+
+docker-compose up --build -d
+docker ps
+
+cd ~/MedGateDeployment
