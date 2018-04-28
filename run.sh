2420a0482e28940f4eca34080d735b6c8f39244c
Simon Thompson <simon@chi.swan.ac.uk>
2018-04-28 23:48:33 +0100

    Updated Run - host IP address

--- run.sh
+++ run.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
+echo '--------------'
+echo '-  MedGATE   -'
+echo '--------------'
+echo ''
+
+export host_ip=$(ip address | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.' )
+# FOR DEMO, easier
+export host_ip=localhost
+
 # stop all the running services
+echo '==> Stopping any running MedGATE services'
 if [[ -s ~/MedGateDeployment/docker/docker-compose.yml ]]
 then
     docker-compose -f ~/MedGateDeployment/docker/docker-compose.yml stop
 fi
 
+echo '==> Configuring enviroment'
 # create gcp related directories for initialization 
 mkdir -p /gcp/instances/gcp_1/in
 mkdir -p /gcp/applications
 mkdir -p /gcp/data
 mkdir -p /gcp/output
 
-
+echo '==> Pulling containers for MedGATE services'
 #remove medgate-service container and pull the latest medgate-service image 
 if [ "$(docker ps -aq -f name='medgate-service')" ]; then
     # cleanup
-    docker-compose rm -f medgate-service
+    # ?? pull should update - so why delete, docker-compose rm -f medgate-service
     # pull the latest image
     docker-compose pull medgate-service
 fi
 
+echo '==> Building Docker Images'
 docker-compose -f ~/MedGateDeployment/docker/docker-compose.yml up --build -d
+
+clear
+echo '==> Running Docker Images'
 docker ps
 
 
