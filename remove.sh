5489b1a626fe4dfdf3ec8909258cb29ff490179a
Simon Thompson <simon@chi.swan.ac.uk>
2018-04-28 23:49:51 +0100

    added comments

--- remove.sh
+++ remove.sh
@@ -1,1 +1,6 @@
+echo '--------------'
+echo '-  MedGATE   -'
+echo '--------------'
+echo ''
+echo '==> Removing MedGATE service'
 docker-compose -f ~/MedGateDeployment/docker/docker-compose.yml rm
--- stop.sh
+++ stop.sh
@@ -1,1 +1,7 @@
+echo '--------------'
+echo '-  MedGATE   -'
+echo '--------------'
+echo ''
+echo '==> Stopping MedGATE service'
+
 docker-compose -f ~/MedGateDeployment/docker/docker-compose.yml stop
