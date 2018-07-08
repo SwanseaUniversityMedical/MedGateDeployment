62a753c2d13b50397b90eaccfc3ac38c1057da3e
Simon Thompson <simon@chi.swan.ac.uk>
2018-07-08 23:07:10 +0100

    adding docker-compose to setup script

--- installdocker.sh
+++ installdocker.sh
@@ -14,3 +14,6 @@ sudo apt-get install -y docker-ce &&
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose &&
 sudo chmod +x /usr/local/bin/docker-compose
 sudo sysctl -w vm.max_map_count=262144
+
+ apt-get install -y docker-compose
+
