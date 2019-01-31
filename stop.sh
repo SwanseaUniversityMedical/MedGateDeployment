f4b3963d59a9862d711a7e9e4f1d9cf54122add3
Jeffrey Peng <kjeffrey1983@gmail.com>
2019-01-31 15:37:08 +0000

    ftp_host_ip and host_ip added to avoid warning msg

--- stop.sh
+++ stop.sh
@@ -4,4 +4,7 @@ echo '--------------'
 echo ''
 echo '==> Stopping MedGATE service'
 
+export ftp_host_ip=$(ip address | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172.' )
+export host_ip=0.0.0.0
+
 docker-compose -f ./docker/docker-compose.yml stop
