dcfc5036803d9624e0c27b5a515eb0f3f8a8e85f
henry.wang <henry@chi.swan.ac.uk>
2019-02-07 14:13:40 +0000

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -5,13 +5,13 @@ echo ''
 echo '==> Resetting MedGATE service'
 
 reset(){
-   sudo sh stop.sh
-   sudo sh remove.sh
+   sh stop.sh
+   sh remove.sh
    echo 'Removing data volumes'
-   sudo docker volume rm docker_esdata docker_pgdata 2>/dev/null
+   docker volume rm docker_esdata docker_pgdata 2>/dev/null
    echo 'Removing local files'
-   sudo rm /gcp /medgate /brat-cfg /brat-data -rf
-   echo '==> Done'
+   rm /gcp /medgate /brat-cfg /brat-data -rf
+   sh run.sh
 }
 
 read -p "WARNING: this will remove everything (including your uploaded documents) associated with the MedGATE instance, continue (y/n)?" choice
